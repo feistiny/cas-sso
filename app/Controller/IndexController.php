185b652dc8c31933da465fc252489ac20342bd94
lzf <liuzhanfei166@126.com>
2022-06-14 23:28:37 +0800

    init

---
+++ app/Controller/IndexController.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace App\Controller;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @var \Hyperf\Contract\SessionInterface
+     */
+    private $session;
+    
+    public function index()
+    {
+        $user = $this->request->input('user', 'Hyperf');
+        $method = $this->request->getMethod();
+        
+        
+
+        return [
+            'method' => $method,
+            'message' => "Hello2 {$user}.",
+        ];
+    }
+}
